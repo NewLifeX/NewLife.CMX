ac1568d31ab4f2e935a7a30f9ab1f25afc62b452
xi3892 <xi3892@163.com>
2013-12-13 15:14:33 +0000

    添加httphandlers 类

---
+++ Src/NewLife.CMX.Web/Handlers/FormRouting.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+
+namespace NewLife.CMX.Web.Handlers
+{
+    public class FormRouting : IHttpHandler
+{
+    public void ProcessRequest(HttpContext context)
+    {
+        if (Admin.Current == null) context.Response.Redirect("Default.aspx");
+
+        //参数频道扩展名（Suffix）
+        Channel channel = Channel.FindBySuffix(context.Request["Channel"]);
+
+        Admin admin = Admin.Current;
+
+        ChannelRole cr = ChannelRole.FindChannelIDAndRoleID(channel.ID, admin.RoleID);
+
+        if (cr == null) context.Response.Redirect("Default.aspx");
+
+        if (channel != null)
+        {
+            String url = channel.Model.FormTemplatePath;
+
+            if (!String.IsNullOrEmpty(url))
+            {
+                url += "?" + context.Request.QueryString.ToString();
+                context.Response.Redirect(url);
+            }
+            else
+            {
+                context.Response.Write("路径地址无法解析");
+            }
+        }
+        context.Response.StatusCode = 404;
+        context.Response.Write("未知地址！");
+        context.Response.End();
+    }
+
+    public bool IsReusable
+    {
+        get
+        {
+            return false;
+        }
+    }
+}
+}
---
+++ Src/NewLife.CMX.Web/Handlers/ListRouting.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+
+namespace NewLife.CMX.Web.Handlers
+{
+    public class ListRouting : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            if (Admin.Current == null) context.Response.Redirect("Default.aspx");
+
+            //参数频道的扩展名（Suffix）
+            Channel c = Channel.FindBySuffix(context.Request["Channel"]);
+
+            Admin admin = Admin.Current;
+
+            ChannelRole cr = ChannelRole.FindChannelIDAndRoleID(c.ID, admin.RoleID);
+
+            if (cr == null) context.Response.Redirect("Default.aspx");
+
+            if (c != null)
+            {
+                String url = c.Model.ListTemplatePath;
+
+                if (String.IsNullOrEmpty(url)) return;
+
+                url += "?" + context.Request.QueryString.ToString();
+                context.Response.Redirect(url);
+            }
+
+            context.Response.StatusCode = 404;
+            context.Response.Write("未知地址！");
+            context.Response.End();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="channelid"></param>
+        public static String GetUrl(Int32 channelid)
+        {
+            Channel c = Channel.FindByID(channelid);
+
+            return c == null ? "" : c.Model.ListTemplatePath;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="channelname"></param>
+        public static String GetUrl(String channelname)
+        {
+            Channel c = Channel.FindBySuffix(channelname);
+
+            return c == null ? "" : c.Model.ListTemplatePath;
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
