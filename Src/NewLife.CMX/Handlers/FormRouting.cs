e700b74beec0c2d78fac3654d8b7102a8978ae9c
xi3892 <xi3892@163.com>
2014-02-27 15:29:05 +0000

    调整后台页面httphandlers类 位置从NewLife.CMX.Web移动到NewLife.CMX中 NewLife.CMX.Web只存放扩展的模板基类

---
+++ Src/NewLife.CMX/Handlers/FormRouting.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using NewLife.Web;
+
+namespace NewLife.CMX.Handlers
+{
+    public class FormRouting : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            if (Admin.Current == null) context.Response.Redirect("Default.aspx");
+
+            var cid = WebHelper.RequestInt("ChannelID");
+            var chn = Channel.FindByID(cid);
+            //Channel chn = Channel.FindBySuffixOrModel(context.Request["Channel"], WebHelper.RequestInt("ModelID"));
+
+            if (chn != null)
+            {
+                ChannelRole cr = ChannelRole.FindChannelIDAndRoleID(chn.ID, Admin.Current.RoleID);
+                if (cr == null) context.Response.Redirect("Default.aspx");
+
+                String url = chn.Model.TitleTemplatePath;
+                if (!String.IsNullOrEmpty(url))
+                {
+                    url += "?" + context.Request.QueryString.ToString();
+                    context.Response.Redirect(url);
+                }
+                else
+                {
+                    context.Response.Write("路径地址无法解析");
+                }
+            }
+            context.Response.StatusCode = 404;
+            context.Response.Write("未知地址！");
+            context.Response.End();
+        }
+
+        public bool IsReusable { get { return false; } }
+    }
+}
---
+++ Src/NewLife.CMX/Handlers/ListRouting.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Web;
+using NewLife.Web;
+
+namespace NewLife.CMX.Handlers
+{
+    public class ListRouting : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            if (Admin.Current == null) context.Response.Redirect("Login.aspx");
+
+            var cid = WebHelper.RequestInt("ChannelID");
+            var chn = Channel.FindByID(cid);
+            ////由于默认频道的存在，默认频道是没有后缀扩展名。所以需要先根据频道扩展名查询，如果没有扩展名在使用模型编号查询
+            //Channel chn = Channel.FindBySuffixOrModel(context.Request["Channel"], WebHelper.RequestInt("ModelID"));
+
+            if (chn != null)
+            {
+                var cr = ChannelRole.FindChannelIDAndRoleID(chn.ID, Admin.Current.RoleID);
+                if (cr == null) context.Response.Redirect("Default.aspx");
+
+                String url = chn.Model.CategoryTemplatePath;
+                if (String.IsNullOrEmpty(url)) return;
+
+                url += "?" + context.Request.QueryString.ToString();
+                context.Response.Redirect(url);
+            }
+
+            context.Response.StatusCode = 404;
+            context.Response.Write("未知地址！");
+            context.Response.End();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="channelid"></param>
+        public static String GetUrl(Int32 channelid)
+        {
+            var chn = Channel.FindByID(channelid);
+
+            return chn == null ? "" : chn.Model.CategoryTemplatePath;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="channelname"></param>
+        public static String GetUrl(String channelname)
+        {
+            var chn = Channel.FindByName(channelname);
+
+            return chn == null ? "" : chn.Model.CategoryTemplatePath;
+        }
+
+        public bool IsReusable { get { return false; } }
+    }
+}
