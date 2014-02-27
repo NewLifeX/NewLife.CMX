d23a6653f9f9d6807a0e1377fc8f39e39aab5ac3
nnhy <nnhy@vip.qq.com>
2014-02-27 04:06:28 +0000

    文章基类

---
+++ Src/NewLife.CMX/Templates/Article.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using XTemplate.Templating;
+
+namespace NewLife.CMX.Templates
+{
+    /// <summary>文章列表模版基类。首页模版生成类继承于此类</summary>
+    public class ArticleListPage : PageBase
+    {
+    }
+
+    /// <summary>文章模版基类。首页模版生成类继承于此类</summary>
+    public class ArticlePage : PageBase
+    {
+        #region 属性
+        private Article _Entity;
+        /// <summary>实体对象</summary>
+        public Article Entity { get { return _Entity; } set { _Entity = value; } }
+        #endregion
+    }
+}
