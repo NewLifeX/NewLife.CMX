c9872522f5bdc944e28a4a58b1be71b3258b7f6f
Stone <nnhy@vip.qq.com>
2014-11-08 17:03:36 +0000

    增加导航实体类

---
+++ Src/NewLife.CMX/站点/导航.cs
@@ -0,0 +1,280 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Xml.Serialization;
+using XCode;
+using XCode.Configuration;
+using XCode.DataAccessLayer;
+
+namespace NewLife.CMX
+{
+    /// <summary>导航</summary>
+    [Serializable]
+    [DataObject]
+    [Description("导航")]
+    [BindTable("Nav", Description = "导航", ConnName = "CMX", DbType = DatabaseType.SqlServer)]
+    public partial class Nav : INav
+    {
+        #region 属性
+        private Int32 _ID;
+        /// <summary>编号</summary>
+        [DisplayName("编号")]
+        [Description("编号")]
+        [DataObjectField(true, true, false, 10)]
+        [BindColumn(1, "ID", "编号", null, "int", 10, 0, false)]
+        public virtual Int32 ID
+        {
+            get { return _ID; }
+            set { if (OnPropertyChanging(__.ID, value)) { _ID = value; OnPropertyChanged(__.ID); } }
+        }
+
+        private String _Name;
+        /// <summary>名称</summary>
+        [DisplayName("名称")]
+        [Description("名称")]
+        [DataObjectField(false, false, true, 50)]
+        [BindColumn(2, "Name", "名称", null, "nvarchar(50)", 0, 0, true)]
+        public virtual String Name
+        {
+            get { return _Name; }
+            set { if (OnPropertyChanging(__.Name, value)) { _Name = value; OnPropertyChanged(__.Name); } }
+        }
+
+        private String _Url;
+        /// <summary>地址</summary>
+        [DisplayName("地址")]
+        [Description("地址")]
+        [DataObjectField(false, false, true, 50)]
+        [BindColumn(3, "Url", "地址", null, "nvarchar(50)", 0, 0, true)]
+        public virtual String Url
+        {
+            get { return _Url; }
+            set { if (OnPropertyChanging(__.Url, value)) { _Url = value; OnPropertyChanged(__.Url); } }
+        }
+
+        private Boolean _NewWindow;
+        /// <summary>是否空白窗口打开</summary>
+        [DisplayName("是否空白窗口打开")]
+        [Description("是否空白窗口打开")]
+        [DataObjectField(false, false, true, 1)]
+        [BindColumn(4, "NewWindow", "是否空白窗口打开", null, "bit", 0, 0, false)]
+        public virtual Boolean NewWindow
+        {
+            get { return _NewWindow; }
+            set { if (OnPropertyChanging(__.NewWindow, value)) { _NewWindow = value; OnPropertyChanged(__.NewWindow); } }
+        }
+
+        private Int32 _CreateUserID;
+        /// <summary>创建人</summary>
+        [DisplayName("创建人")]
+        [Description("创建人")]
+        [DataObjectField(false, false, true, 10)]
+        [BindColumn(5, "CreateUserID", "创建人", null, "int", 10, 0, false)]
+        public virtual Int32 CreateUserID
+        {
+            get { return _CreateUserID; }
+            set { if (OnPropertyChanging(__.CreateUserID, value)) { _CreateUserID = value; OnPropertyChanged(__.CreateUserID); } }
+        }
+
+        private DateTime _CreateTime;
+        /// <summary>创建时间</summary>
+        [DisplayName("创建时间")]
+        [Description("创建时间")]
+        [DataObjectField(false, false, true, 3)]
+        [BindColumn(6, "CreateTime", "创建时间", null, "datetime", 3, 0, false)]
+        public virtual DateTime CreateTime
+        {
+            get { return _CreateTime; }
+            set { if (OnPropertyChanging(__.CreateTime, value)) { _CreateTime = value; OnPropertyChanged(__.CreateTime); } }
+        }
+
+        private Int32 _UpdateUserID;
+        /// <summary>更新人</summary>
+        [DisplayName("更新人")]
+        [Description("更新人")]
+        [DataObjectField(false, false, true, 10)]
+        [BindColumn(7, "UpdateUserID", "更新人", null, "int", 10, 0, false)]
+        public virtual Int32 UpdateUserID
+        {
+            get { return _UpdateUserID; }
+            set { if (OnPropertyChanging(__.UpdateUserID, value)) { _UpdateUserID = value; OnPropertyChanged(__.UpdateUserID); } }
+        }
+
+        private DateTime _UpdateTime;
+        /// <summary>更新时间</summary>
+        [DisplayName("更新时间")]
+        [Description("更新时间")]
+        [DataObjectField(false, false, true, 3)]
+        [BindColumn(8, "UpdateTime", "更新时间", null, "datetime", 3, 0, false)]
+        public virtual DateTime UpdateTime
+        {
+            get { return _UpdateTime; }
+            set { if (OnPropertyChanging(__.UpdateTime, value)) { _UpdateTime = value; OnPropertyChanged(__.UpdateTime); } }
+        }
+
+        private String _Remark;
+        /// <summary>备注</summary>
+        [DisplayName("备注")]
+        [Description("备注")]
+        [DataObjectField(false, false, true, 200)]
+        [BindColumn(9, "Remark", "备注", null, "nvarchar(200)", 0, 0, true)]
+        public virtual String Remark
+        {
+            get { return _Remark; }
+            set { if (OnPropertyChanging(__.Remark, value)) { _Remark = value; OnPropertyChanged(__.Remark); } }
+        }
+        #endregion
+
+        #region 获取/设置 字段值
+        /// <summary>
+        /// 获取/设置 字段值。
+        /// 一个索引，基类使用反射实现。
+        /// 派生实体类可重写该索引，以避免反射带来的性能损耗
+        /// </summary>
+        /// <param name="name">字段名</param>
+        /// <returns></returns>
+        public override Object this[String name]
+        {
+            get
+            {
+                switch (name)
+                {
+                    case __.ID : return _ID;
+                    case __.Name : return _Name;
+                    case __.Url : return _Url;
+                    case __.NewWindow : return _NewWindow;
+                    case __.CreateUserID : return _CreateUserID;
+                    case __.CreateTime : return _CreateTime;
+                    case __.UpdateUserID : return _UpdateUserID;
+                    case __.UpdateTime : return _UpdateTime;
+                    case __.Remark : return _Remark;
+                    default: return base[name];
+                }
+            }
+            set
+            {
+                switch (name)
+                {
+                    case __.ID : _ID = Convert.ToInt32(value); break;
+                    case __.Name : _Name = Convert.ToString(value); break;
+                    case __.Url : _Url = Convert.ToString(value); break;
+                    case __.NewWindow : _NewWindow = Convert.ToBoolean(value); break;
+                    case __.CreateUserID : _CreateUserID = Convert.ToInt32(value); break;
+                    case __.CreateTime : _CreateTime = Convert.ToDateTime(value); break;
+                    case __.UpdateUserID : _UpdateUserID = Convert.ToInt32(value); break;
+                    case __.UpdateTime : _UpdateTime = Convert.ToDateTime(value); break;
+                    case __.Remark : _Remark = Convert.ToString(value); break;
+                    default: base[name] = value; break;
+                }
+            }
+        }
+        #endregion
+
+        #region 字段名
+        /// <summary>取得导航字段信息的快捷方式</summary>
+        public partial class _
+        {
+            ///<summary>编号</summary>
+            public static readonly Field ID = FindByName(__.ID);
+
+            ///<summary>名称</summary>
+            public static readonly Field Name = FindByName(__.Name);
+
+            ///<summary>地址</summary>
+            public static readonly Field Url = FindByName(__.Url);
+
+            ///<summary>是否空白窗口打开</summary>
+            public static readonly Field NewWindow = FindByName(__.NewWindow);
+
+            ///<summary>创建人</summary>
+            public static readonly Field CreateUserID = FindByName(__.CreateUserID);
+
+            ///<summary>创建时间</summary>
+            public static readonly Field CreateTime = FindByName(__.CreateTime);
+
+            ///<summary>更新人</summary>
+            public static readonly Field UpdateUserID = FindByName(__.UpdateUserID);
+
+            ///<summary>更新时间</summary>
+            public static readonly Field UpdateTime = FindByName(__.UpdateTime);
+
+            ///<summary>备注</summary>
+            public static readonly Field Remark = FindByName(__.Remark);
+
+            static Field FindByName(String name) { return Meta.Table.FindByName(name); }
+        }
+
+        /// <summary>取得导航字段名称的快捷方式</summary>
+        partial class __
+        {
+            ///<summary>编号</summary>
+            public const String ID = "ID";
+
+            ///<summary>名称</summary>
+            public const String Name = "Name";
+
+            ///<summary>地址</summary>
+            public const String Url = "Url";
+
+            ///<summary>是否空白窗口打开</summary>
+            public const String NewWindow = "NewWindow";
+
+            ///<summary>创建人</summary>
+            public const String CreateUserID = "CreateUserID";
+
+            ///<summary>创建时间</summary>
+            public const String CreateTime = "CreateTime";
+
+            ///<summary>更新人</summary>
+            public const String UpdateUserID = "UpdateUserID";
+
+            ///<summary>更新时间</summary>
+            public const String UpdateTime = "UpdateTime";
+
+            ///<summary>备注</summary>
+            public const String Remark = "Remark";
+
+        }
+        #endregion
+    }
+
+    /// <summary>导航接口</summary>
+    public partial interface INav
+    {
+        #region 属性
+        /// <summary>编号</summary>
+        Int32 ID { get; set; }
+
+        /// <summary>名称</summary>
+        String Name { get; set; }
+
+        /// <summary>地址</summary>
+        String Url { get; set; }
+
+        /// <summary>是否空白窗口打开</summary>
+        Boolean NewWindow { get; set; }
+
+        /// <summary>创建人</summary>
+        Int32 CreateUserID { get; set; }
+
+        /// <summary>创建时间</summary>
+        DateTime CreateTime { get; set; }
+
+        /// <summary>更新人</summary>
+        Int32 UpdateUserID { get; set; }
+
+        /// <summary>更新时间</summary>
+        DateTime UpdateTime { get; set; }
+
+        /// <summary>备注</summary>
+        String Remark { get; set; }
+        #endregion
+
+        #region 获取/设置 字段值
+        /// <summary>获取/设置 字段值。</summary>
+        /// <param name="name">字段名</param>
+        /// <returns></returns>
+        Object this[String name] { get; set; }
+        #endregion
+    }
+}
