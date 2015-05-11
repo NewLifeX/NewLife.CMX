5f91f9cc3fb9a049f2e571fdd9595638059a1f07
Stone <nnhy@vip.qq.com>
2015-05-11 19:41:46 +0000

    除标题外，内容系统的其它类不显示菜单

--- Src/NewLife.CMX.Web/Areas/CMS/Common/CategoryController.cs
+++ Src/NewLife.CMX.Web/Areas/CMS/Common/CategoryController.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using NewLife.Cube;
 using XCode;
+using XCode.Membership;
 
 namespace NewLife.CMX.Web
 {
     public class CategoryController<TEntity> : EntityController<TEntity> where TEntity : EntityCategory<TEntity>, new()
     {
+        protected override IDictionary<MethodInfo, int> ScanActionMenu(IMenu menu)
+        {
+            menu.Visible = false;
+
+            return base.ScanActionMenu(menu);
+        }
     }
 }
