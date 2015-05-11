64ac0f9df79c3494015874579fb311d5afc524c5
Stone <nnhy@vip.qq.com>
2015-05-11 18:43:33 +0000

    创建Mvc项目

--- Src/NewLife.CMX.Web/Properties/AssemblyInfo.cs
+++ Src/NewLife.CMX.Web/Properties/AssemblyInfo.cs
@@ -2,35 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// 有关程序集的常规信息通过以下
-// 特性集控制。更改这些特性值可修改
+// 有关程序集的常规信息通过下列特性集
+// 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("NewLife.CMX.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("NewLife.CMX.Web")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("版权所有(C) Microsoft 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// 将 ComVisible 设置为 false 使此程序集中的类型
-// 对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型，
-// 则将该类型上的 ComVisible 特性设置为 true。
+// 将 ComVisible 设置为 false 会使此程序集中的类型
+// 对 COM 组件不可见。如果需要从 COM 访问此程序集中的某个类型，
+// 请针对该类型将 ComVisible 特性设置为 true。
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("e72250ff-de07-4903-b6a4-046b85a1345e")]
+[assembly: Guid("f58a77bd-ff76-422a-b309-232378c588f5")]
 
-// 程序集的版本信息由下面四个值组成:
+// 程序集的版本信息由下列四个值组成:
 //
 //      主版本
-//      次版本 
-//      生成号
+//      次版本
+//      内部版本号
 //      修订号
 //
-// 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
+// 可以指定所有这些值，也可以使用“修订号”和“内部版本号”的默认值，
 // 方法是按如下所示使用“*”:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
