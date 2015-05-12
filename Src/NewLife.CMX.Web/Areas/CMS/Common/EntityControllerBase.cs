460efeafd2c6956f661ace76fc0b763a1072f7db
Stone <nnhy@vip.qq.com>
2015-05-12 19:28:55 +0000

    使用控制器指定的字段列表

--- Src/NewLife.CMX.Web/Areas/CMS/Common/EntityControllerBase.cs
+++ Src/NewLife.CMX.Web/Areas/CMS/Common/EntityControllerBase.cs
@@ -20,8 +20,29 @@ namespace NewLife.CMX.Web
         protected override ActionResult IndexView(Pager p)
         {
             var fields = ViewBag.Fields as List<FieldItem>;
-            var fs = Entity<TEntity>.Meta.AllFields;
 
+            FieldFilter(fields);
+
+            return base.IndexView(p);
+        }
+
+        /// <summary>表单页视图。子控制器可以重载，以传递更多信息给视图，比如修改要显示的列</summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        protected override ActionResult FormView(TEntity entity)
+        {
+            var fields = ViewBag.Fields as List<FieldItem>;
+            if (fields == null) ViewBag.Fields = fields = Entity<TEntity>.Meta.Fields.ToList();
+
+            FieldFilter(fields);
+
+            return base.FormView(entity);
+        }
+
+        void FieldFilter(List<FieldItem> fields)
+        {
+            var fs = Entity<TEntity>.Meta.AllFields;
+            
             for (int i = 0; i < fields.Count; i++)
             {
                 if (fields[i].Name.EqualIgnoreCase("CreateUserID"))
@@ -39,8 +60,6 @@ namespace NewLife.CMX.Web
                         fields.RemoveAt(i--);
                 }
             }
-
-            return base.IndexView(p);
         }
     }
 }
