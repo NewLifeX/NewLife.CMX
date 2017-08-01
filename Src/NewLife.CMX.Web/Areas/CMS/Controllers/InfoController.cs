a7060c0b8f9639cbcb9b4d77270571563daea7e0
大石头 <nnhy@vip.qq.com>
2017-08-02 07:58:44 +0800

    支持统一查看所有信息

--- Src/NewLife.CMX.Web/Areas/CMS/Controllers/InfoController.cs
+++ Src/NewLife.CMX.Web/Areas/CMS/Controllers/InfoController.cs
@@ -25,12 +25,12 @@ namespace NewLife.CMX.Web.Controllers
             if (fi != null) list.Add(fi);
         }
 
-        protected override IDictionary<MethodInfo, Int32> ScanActionMenu(IMenu menu)
-        {
-            menu.Visible = false;
+        //protected override IDictionary<MethodInfo, Int32> ScanActionMenu(IMenu menu)
+        //{
+        //    menu.Visible = false;
 
-            return base.ScanActionMenu(menu);
-        }
+        //    return base.ScanActionMenu(menu);
+        //}
 
         /// <summary>列表页视图。子控制器可重载，以传递更多信息给视图，比如修改要显示的列</summary>
         /// <param name="p"></param>
